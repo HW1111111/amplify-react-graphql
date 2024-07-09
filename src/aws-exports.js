20cd29d2dc920e5452abde986d78c2c56b8f8055
wuhaonan <wuhaonan923@gmail.com>
2024-07-09 22:28:10 +0800

    update

--- src/aws-exports.js
+++ src/aws-exports.js
@@ -23,7 +23,13 @@ const awsmobile = {
     },
     "aws_cognito_verification_mechanisms": [
         "EMAIL"
-    ]
+    ],
+    "aws_appsync_graphqlEndpoint": "https://meihtunfzrc35ferjd6hiltzkm.appsync-api.us-east-2.amazonaws.com/graphql",
+    "aws_appsync_region": "us-east-2",
+    "aws_appsync_authenticationType": "API_KEY",
+    "aws_appsync_apiKey": "da2-tdv5wna7gzawlfj25julye62tm",
+    "aws_user_files_s3_bucket": "amplifyapp4ca09c6b04a54a82aa6ea40d36ea5bb8af2ff-dev",
+    "aws_user_files_s3_bucket_region": "us-east-2"
 };
 
 
